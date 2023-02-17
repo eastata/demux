75ff310d883508f3fb70830329063e39fddaadd8
Aleksandr Poliakov <81574018+eastata-vg@users.noreply.github.com>
2023-02-17 18:48:12 +0000

    Add simple draft demux implementation

--- cmd/demux/main.go
+++ cmd/demux/main.go
@@ -1,11 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"math/rand"
+	"runtime"
+	"sync"
+)
 
-func main() {
-	fmt.Println("Ololo", placeholder(1))
+type job struct {
+	id   uuid.UUID
+	data []int
 }
 
-func placeholder(x int) int {
-	return x
+func main() {
+	jobslist := jobsGenerator()
+	scheduler(jobslist)
+}
+
+func jobsGenerator() []job {
+	var (
+		jb job
+		id uuid.UUID
+	)
+
+	// uuid.EnableRandPool() are not thread-safe and should only be called when there is no possibility that uuid.New()
+	// generation function will be called concurrently.
+	uuid.EnableRandPool()
+
+	jobs := make([]job, 0)
+	for i := 0; i < 100; i++ {
+		id = uuid.New()
+		jb = job{id, []int{rand.Intn(100), rand.Intn(100)}}
+		jobs = append(jobs, jb)
+	}
+	return jobs
+}
+
+func scheduler(joblist []job) {
+	//cpu := runtime.NumCPU()
+	//fmt.Println("Num CPU: ", cpu)
+
+	wg := sync.WaitGroup{}
+
+	for _, v := range joblist {
+		wg.Add(1)
+		go worker(&wg, v)
+	}
+	fmt.Println("Num Goroutine: ", runtime.NumGoroutine())
+
+	wg.Wait()
+	fmt.Println("Num Goroutine: ", runtime.NumGoroutine())
+}
+
+func worker(w *sync.WaitGroup, jb job) error {
+	sum := 0
+	for _, v := range jb.data {
+		sum += v
+	}
+	runtime.Gosched()
+	fmt.Println("Job: ", jb.id.String(), "\tSUM: ", sum)
+	w.Done()
+	return nil
 }
--- cmd/demux/main_test.go
+++ cmd/demux/main_test.go
@@ -1,17 +1,54 @@
 package main
 
-import "testing"
+import (
+	"github.com/google/uuid"
+	"sync"
+	"testing"
+)
 
-func TestPlaceholder(t *testing.T) {
-	x := 10
-	got := placeholder(x)
-	if got != 10 {
-		t.Errorf("Expected: %v\t Got: %v", 10, got)
+func TestJobsGenerator(t *testing.T) {
+	jobs := jobsGenerator()
+	if jobs[0].id == jobs[2].id {
+		t.Errorf("Collision for generated jobs UUID")
 	}
 }
 
-func BenchmarkPlaceholder(b *testing.B) {
+func BenchmarkJonsGenerator(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		placeholder(10)
+		jobsGenerator()
+	}
+}
+
+func TestShceduler(t *testing.T) {
+	jobs := jobsGenerator()
+	scheduler(jobs)
+}
+
+func BenchmarkScheduler(b *testing.B) {
+	jobs := jobsGenerator()
+	for i := 0; i < b.N; i++ {
+		scheduler(jobs)
+	}
+}
+
+func TestWorker(t *testing.T) {
+	jb := job{id: uuid.New(), data: []int{1, 2}}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	err := worker(&wg, jb)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	wg.Wait()
+}
+
+func BenchmarkWorker(b *testing.B) {
+	jb := job{id: uuid.New(), data: []int{1, 2}}
+	for i := 0; i < b.N; i++ {
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		worker(&wg, jb)
+		wg.Wait()
 	}
 }
