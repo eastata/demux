5d195d4c10b6d2abac1d7f163c33d0a0db3148ac
Aleksandr Poliakov <aleksandr.poliakov@vonage.com>
2023-02-28 13:59:40 +0000

    Refactoring pkg structure. Stage 1

---
+++ pkg/demux/main.go
@@ -0,0 +1,62 @@
+package demux
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"math/rand"
+	"runtime"
+)
+
+type job struct {
+	id   uuid.UUID
+	data []int
+}
+
+type jobResponse struct {
+	id  uuid.UUID
+	out int
+}
+
+func JobsGenerator() []job {
+	var (
+		jb job
+		id uuid.UUID
+	)
+
+	// uuid.EnableRandPool() are not thread-safe and should only be called when there is no possibility that uuid.New()
+	// generation function will be called concurrently.
+	uuid.EnableRandPool()
+
+	jobs := make([]job, 0)
+	for i := 0; i < 100; i++ {
+		id = uuid.New()
+		jb = job{id, []int{rand.Intn(100), rand.Intn(100)}}
+		jobs = append(jobs, jb)
+	}
+	return jobs
+}
+
+func Scheduler(joblist []job) {
+
+	ch := make(chan jobResponse)
+	for _, v := range joblist {
+		go worker(ch, v)
+	}
+
+	for i := 0; i < len(joblist); i++ {
+		//There is a jobs response output
+		fmt.Println(<-ch)
+		<-ch
+	}
+
+}
+
+func worker(ch chan<- jobResponse, jb job) error {
+	resp := jobResponse{id: jb.id, out: 0}
+	for _, v := range jb.data {
+		resp.out += v
+	}
+	runtime.Gosched()
+	ch <- resp
+	return nil
+}
---
+++ pkg/demux/main_test.go
@@ -0,0 +1,59 @@
+package demux
+
+import (
+	"github.com/google/uuid"
+	"testing"
+)
+
+func TestJobsGenerator(t *testing.T) {
+	jobs := JobsGenerator()
+	if jobs[0].id == jobs[2].id {
+		t.Errorf("Collision for generated jobs UUID")
+	}
+}
+
+func BenchmarkJonsGenerator(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		JobsGenerator()
+	}
+}
+
+func TestShceduler(t *testing.T) {
+	jobs := JobsGenerator()
+	Scheduler(jobs)
+}
+
+func BenchmarkScheduler(b *testing.B) {
+	jobs := JobsGenerator()
+	for i := 0; i < b.N; i++ {
+		Scheduler(jobs)
+	}
+}
+
+func TestWorker(t *testing.T) {
+	jb := job{id: uuid.New(), data: []int{1, 2}}
+
+	ch := make(chan jobResponse, 1)
+
+	err := worker(ch, jb)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	close(ch)
+}
+
+func BenchmarkWorker(b *testing.B) {
+	jb := job{id: uuid.New(), data: []int{1, 2}}
+	ch := make(chan jobResponse)
+
+	go func() {
+		for i := 0; i < b.N; i++ {
+			<-ch
+		}
+	}()
+
+	for i := 0; i < b.N; i++ {
+		worker(ch, jb)
+
+	}
+}
