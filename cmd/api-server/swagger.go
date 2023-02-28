878947d6c72776a95ef3a40edcc435d3b22712ce
Aleksandr Poliakov <aleksandr.poliakov@vonage.com>
2023-02-28 16:44:13 +0000

    Refactoring pkg structure. Stage 5

---
+++ cmd/api-server/swagger.go
@@ -0,0 +1,11 @@
+//	Comapany Eastata:
+//	 version: 0.0.1
+//	 title: Demux API
+//	Schemes: http, https
+//	Host: localhost:8080
+//	BasePath: /
+//	Produces:
+//	  - application/json
+//
+// swagger:meta
+package main
